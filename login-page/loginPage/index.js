41434bee4c4cb32b2814642c80615715f287d949
Joseph Abraham <ekurubal24@gmail.com>
2020-10-12 13:07:06 +0300

    Comleted files

---
+++ login-page/loginPage/index.js
@@ -0,0 +1,21 @@
+// /**
+//  * @format
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import Login from './src/Login';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => Login);
+
+  
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Login from './src/Login';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Login);
+
